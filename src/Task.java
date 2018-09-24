1371b883a1a1220450a4731e5bc2cffef6a69a0a
tlhamo <tsekilhamo@gmail.com>
2018-09-24 18:19:01 -0500

    Add files via upload

---
+++ src/Task.java
@@ -0,0 +1,14 @@
+
+/**
+ * A Task object contains a String (for task description) and a integer (converted from date)
+ * 
+ * @author Sabirah Shuaybi
+ * @author Tseki Lhamo
+ */
+public class Task {
+	
+	public Task() {
+		//TODO create fields
+	}
+
+}
